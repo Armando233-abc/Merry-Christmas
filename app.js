5143fe1bc42335cb1f58930144de36c4180f3a4a
Armando233-abc <armandovigliotti5@gmail.com>
2020-12-18 18:17:39 +0100

    text bug fixed

--- app.js
+++ app.js
@@ -28,22 +28,6 @@ function apertura_occhio(eye){
 }
 
 
-function chiusura_occhio(eye){
-    for(let i = 30; i > 2; i--){
-        eye.style.height = i + "px"
-    }
-    
-    eye.style.width = "30px"
-    eye.style.borderRadius = "0 0 500% 500%"
-    eye.style.background = "#000"
-    eye.style.border = "4px solid black"
-    eye.style.marginTop = "25px"
-
-    let bocca = document.querySelector(".mouth")
-    bocca.style.animation = "mouth 1.3s infinite"
-}
-
-
 function pupilla_visibile(pupil){
     pupil.style.height = "12px"
     pupil.style.width = "12px"
@@ -53,9 +37,6 @@ function pupilla_visibile(pupil){
 }
 
 
-function pupilla_invisibile(pupil){
-    pupil.style.height = "0px"
-}
 
 
 function open(){
@@ -72,23 +53,10 @@ function open(){
     testo.innerHTML = "Buon Natale"
 }
 
-// forse la userò 
-function close(){
-    let left_eye = document.querySelector(".sinistro")
-    let right_eye = document.querySelector(".destro")
-    let left_pupil = document.querySelector(".left")
-    let right_pupil = document.querySelector(".right")
 
-    chiusura_occhio(left_eye)
-    chiusura_occhio(right_eye)
-
-    pupilla_invisibile(left_pupil)
-    pupilla_invisibile(right_pupil)
-}
 
 if (data()){
     open()
-    //close()
 }else{
     eye_part.addEventListener("mouseover", ()=>{
         const testo = document.querySelector(".text")
